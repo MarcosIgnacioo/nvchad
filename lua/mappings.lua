fdb52ff0c08fb0e56c3723b9139f0b4aca48f572
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-07-01 12:46:54 -0700

    yay

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,8 +7,18 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 map("n", "<C-t>", "<M-i>", { desc = "Terminal" })
 map("i", "kj", "<ESC>")
+map("i", "<C-->", "_")
+map("i", "<C-[>", "<")
+map("i", "<C-]>", ">")
+map("i", "<C-0>", "_")
+map("i", "<C-'>", "-")
+map('i', '<C-,>', '<', { noremap = true, silent = true })
+map('i', '<C-.>', '>', { noremap = true, silent = true })
+map('i', '<C-;>', '_', { noremap = true, silent = true })
 map("v", "kj", "<ESC>")
+map("v", "5", "%")
 map("n", "<Leader>a", "<C-^>")
+--kirby
 map("v", "L", ":s/\\(.*\\)/\\1")
 map("n", "<Leader>c", "o```c```<Esc>0fca<CR><Esc><S-o><Esc>p")
 map("i", "<C-c>", "```c```<Esc>0fca<CR><Esc><S-o><Esc>p")
@@ -53,15 +63,14 @@ map('n', '<leader>o', 'ci{<CR><Esc>ko', { noremap = true })
 -- map('n', '<C-w>', 'mAggvG=i<Esc>\'A', { noremap = true, silent = true })
 map('n', '<leader>z', ':ZenMode<CR>', { noremap = true, silent = true })
 map('n', '<leader>n', ':Telescope live_grep<CR>', { noremap = true, silent = true })
-map('i', '<C-;>', 'System.out.println();<Esc>hi', { noremap = true, silent = true })
+-- map('i', '<C-;>', 'System.out.println();<Esc>hi', { noremap = true, silent = true })
 map('n', '<leader>v', ':GoImports<CR>', { noremap = true, silent = true })
-map('i', '<D-,>', [[fmt.Println()<Esc>ha]], { noremap = true, silent = true })
-map('i', '<C-,>', [[fmt.Println()<Esc>ha]], { noremap = true, silent = true })
+map('i', '<D-/>', [[fmt.Println()<Esc>:GoImports<CR>ha]], { noremap = true, silent = true })
+map('i', '<C-/>', [[fmt.Println()<Esc>:GoImports<CR>ha]], { noremap = true, silent = true })
+
 map('n', '<C-;>', 'mA:Telescope lsp_definitions<CR>', { noremap = true, silent = true })
-map('i', '<D-.>', [[if err != nil {<CR><Tab>return err<CR>}<Esc>k]],
- { noremap = true, silent = true })
-map('i', '<C-.>', [[if err != nil {<CR><Tab>return err<CR>}<Esc>k]],
- { noremap = true, silent = true })
+-- map('i', '<D-.>', [[if err != nil {<CR><Tab>return err<CR>}<Esc>k]],
+--   { noremap = true, silent = true })
 map('i', 'kj', '<Esc>', { noremap = true, silent = true })
 map('v', 'kj', '<Esc>', { noremap = true, silent = true })
 map('n', '<Leader>a', '<C-^>', { noremap = true, silent = true })
@@ -107,31 +116,31 @@ for i = 0, 25 do vim.keymap.set("n", "'" .. upp(i), "'" .. low(i)) end
 map('v', '<', '<gv', { noremap = true })
 map('v', '>', '>gv', { noremap = true })
 map({ "n", "t" }, "<leader>'", function()
- require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
 end, { desc = "Terminal Toggle Floating term" })
 -- map({ "n", "t" }, "<C-n>", function()
 --     require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
 -- end, { desc = "Terminal Toggle Floating term" })
 
 map("n", "<leader>q", function()
- require("nvchad.tabufline").close_buffer()
+  require("nvchad.tabufline").close_buffer()
 end, { desc = "Buffer Close" })
 
 
 map("n", "<D-j>", function()
- require("nvchad.tabufline").prev()
+  require("nvchad.tabufline").prev()
 end, { desc = "Buffer Goto prev" })
 
 map("n", "<D-k>", function()
- require("nvchad.tabufline").next()
+  require("nvchad.tabufline").next()
 end, { desc = "Buffer Goto next" })
 
 map("i", "<D-k>", function()
- require("nvchad.tabufline").next()
+  require("nvchad.tabufline").next()
 end, { desc = "Buffer Goto next" })
 
 map("i", "<D-j>", function()
- require("nvchad.tabufline").prev()
+  require("nvchad.tabufline").prev()
 end, { desc = "Buffer Goto prev" })
 
 -- quick fix list prev and next
@@ -139,16 +148,16 @@ vim.keymap.set("n", "<leader>;", "<cmd>cnext<CR>zz", { desc = "Forward qfixlist"
 vim.keymap.set("n", "<leader>h", "<cmd>cprev<CR>zz", { desc = "Backward qfixlist" })
 
 vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
- desc = "Toggle Spectre"
+  desc = "Toggle Spectre"
 })
 vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
- desc = "Search current word"
+  desc = "Search current word"
 })
 vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
- desc = "Search current word"
+  desc = "Search current word"
 })
 vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
- desc = "Search on current file"
+  desc = "Search on current file"
 })
 
 vim.keymap.set("n", "<C-h>", "<C-w>h")
--- lua/options.lua
+++ lua/options.lua
@@ -9,45 +9,45 @@ o.autoindent = true
 o.smartindent = true
 o.indentexpr = "autoindent"
 o.relativenumber = true
-o.tabstop = 1
-o.shiftwidth = 1
-o.softtabstop = 1
+o.tabstop = 2
+o.shiftwidth = 2
+o.softtabstop = 2
 o.incsearch = false
 o.breakindent = true
 o.cursorlineopt = 'both' -- to enable cursorline!
 
 local cmp = require("cmp")
 cmp.setup({
-  mapping = {
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        local entry = cmp.get_selected_entry()
-        if not entry then
-          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-        end
-        cmp.confirm()
-      else
-        fallback()
-      end
-    end, { "i", "s", "c", }),
-    ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-  }
+ mapping = {
+  ["<Tab>"] = cmp.mapping(function(fallback)
+   if cmp.visible() then
+    local entry = cmp.get_selected_entry()
+    if not entry then
+     cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+    end
+    cmp.confirm()
+   else
+    fallback()
+   end
+  end, { "i", "s", "c", }),
+  ['<C-j>'] = cmp.mapping.select_next_item(),
+  ['<C-k>'] = cmp.mapping.select_prev_item(),
+ }
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  group = vim.api.nvim_create_augroup("TS_add_missing_imports", { clear = true }),
-  desc = "TS_add_missing_imports",
-  pattern = { "*.ts", "*.tsx", ".js", ".jsx" },
-  callback = function()
-    vim.lsp.buf.code_action({
-      apply = true,
-      context = {
-        only = { "source.addMissingImports.ts" },
-      },
-    })
-    vim.cmd("write")
-  end,
+ group = vim.api.nvim_create_augroup("TS_add_missing_imports", { clear = true }),
+ desc = "TS_add_missing_imports",
+ pattern = { "*.ts", "*.tsx", ".js", ".jsx" },
+ callback = function()
+  vim.lsp.buf.code_action({
+   apply = true,
+   context = {
+    only = { "source.addMissingImports.ts" },
+   },
+  })
+  vim.cmd("write")
+ end,
 })
 
 -- vim.api.nvim_create_autocmd("Buf", {
